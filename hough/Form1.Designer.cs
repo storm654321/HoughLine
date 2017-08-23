45e8992ee94a56ffefe5ab04856a34f8fb934170
storm654321 <storm654321@gmail.com>
2017-08-23 13:38:03 +0800

    加入專案檔案。

---
+++ hough/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace hough
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "載入圖片";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 88);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(562, 648);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(597, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "垂直";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(597, 54);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "水平";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(690, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(93, 65);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Sobel";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(789, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(93, 65);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Sobel (黑線)";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(597, 88);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(562, 648);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(1066, 17);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(93, 65);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "霍夫直線";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1533, 748);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ hough/Form1.cs
@@ -0,0 +1,414 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+namespace hough
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Bitmap a, b, c;
+        Rectangle clone;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileDialog inputimg;
+            inputimg =new OpenFileDialog();
+            if(inputimg.ShowDialog()==DialogResult.OK)
+            {
+                a = new Bitmap(inputimg.FileName);
+                pictureBox1.Image = a;
+                clone = new Rectangle(0, 0, a.Width, a.Height);
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (a == null)
+            {
+                MessageBox.Show("請先載入圖片");
+                return;
+            }
+            int[,,] source = GetRGBData(a);   
+            int[] filterValue = { -1, -2, -1, 0, 0, 0, 1, 2, 1 };
+            int[,,] newimg = tomatoFilter(filterValue,source);
+            b= setRGBData(newimg);
+            pictureBox2.Image = b;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (a == null)
+            {
+                MessageBox.Show("請先載入圖片");
+                return;
+            }
+            int[,,] source = GetRGBData(a);
+            int[] filterValue = { -1, 0, 1, -2, 0, 2, -1, 0, 1 };
+            int[,,] newimg = tomatoFilter(filterValue, source);
+            b = setRGBData(newimg);
+            pictureBox2.Image = b;
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (a == null)
+            {
+                MessageBox.Show("請先載入圖片");
+                return;
+            }
+            int[,,] source = GetRGBData(a);
+
+            int[] filterValue = { -1, 0, 1, -2, 0, 2, -1, 0, 1 };
+            int[,,] newimg1 = tomatoFilter(filterValue, source);
+
+            int[] filterValue2 = { -1, -2, -1, 0, 0, 0, 1, 2, 1 };
+            int[,,] newimg2 = tomatoFilter(filterValue2, source);
+            int temp;
+            for(int i=0;i<a.Width-1;i++)
+            {
+                for(int j=0;j<a.Height;j++)
+                {
+                    temp = newimg1[i, j, 0] + newimg2[i, j, 0];
+                    if (temp > 255)
+                        temp = 255;
+                    if (temp < 0)
+                        temp = 0;
+                    for(int p=0;p<3;p++)
+                        newimg1[i, j, p] = temp;
+                }
+            }
+
+            b = setRGBData(newimg1);
+            pictureBox2.Image = b;
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (a == null)
+            {
+                MessageBox.Show("請先載入圖片");
+                return;
+            }
+            int[,,] source = GetRGBData(a);
+
+
+            int[] filterValue = { -1, 0, 1, -2, 0, 2, -1, 0, 1 };
+            int[,,] newimg1 = tomatoFilter(filterValue, source);
+
+            int[] filterValue2 = { -1, -2, -1, 0, 0, 0, 1, 2, 1 };
+            int[,,] newimg2 = tomatoFilter(filterValue2, source);
+            int temp;
+            for (int i = 1; i < a.Width - 1; i++)
+            {
+                for (int j = 1; j < a.Height; j++)
+                {
+                    temp = newimg1[i, j, 0] + newimg2[i, j, 0];
+                    if (temp > 255)
+                        temp = 255;
+                    if (temp < 0)
+                        temp = 0;
+                    for (int p = 0; p < 3; p++)
+                        newimg1[i, j, p] = 255-temp;
+                }
+            }
+
+            b = setRGBData(newimg1);
+            pictureBox2.Image = b;
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (a == null)
+            {
+                MessageBox.Show("請先載入圖片");
+                return;
+            }
+            int[,,] source = GetRGBData(a);
+            source = meanFilter(source);
+            int[] filterValue = { -1, 0, 1, -2, 0, 2, -1, 0, 1 };
+            int[,,] newimg1 = tomatoFilter(filterValue, source);
+
+            int[] filterValue2 = { -1, -2, -1, 0, 0, 0, 1, 2, 1 };
+            int[,,] newimg2 = tomatoFilter(filterValue2, source);
+            int temp;
+            int bitemp = 40;   //二質化的門檻
+            for (int i = 0; i < a.Width - 1; i++)
+            {
+                for (int j = 0; j < a.Height; j++)
+                {
+                    temp = newimg1[i, j, 0] + newimg2[i, j, 0];
+                    if (temp > bitemp)
+                        temp = 255;
+                    if (temp <= bitemp)
+                        temp = 0;
+                    for (int p = 0; p < 3; p++)
+                        newimg1[i, j, p] = temp;
+                }
+            }
+            //開始畫parameter  //newimg1=二直圖  
+            int range = Convert.ToInt32(Math.Sqrt(a.Width * a.Width + a.Height * a.Height)); //半徑rhoMax
+            int[,] scouringPad = new int[range * 2 + 1, 181];
+            int rho;
+            
+            for (int i = 0; i < a.Height; i++)
+            {
+                for (int j = 0; j < a.Width; j++)
+                {
+                    if (newimg1[j, i, 0] == 255)
+                    {
+                        //開始畫parameter的圖
+                        for (int ti = 0; ti < 181; ti++)
+                        {
+                            rho = Convert.ToInt16( Math.Cos(ti - 90)*i + Math.Sin(ti - 90)*j);
+                            scouringPad[rho + range, ti]++;
+                        }
+                    }
+                }
+            }
+            int xx=0,yy=0;
+            int rhoMax = 0, totalMax = 0;
+            int thetaMax=100;
+            for (int ii = 0; ii < 1 + 2 * range; ii++)
+            {
+                for (int j = 0; j < 181; j++)
+                {
+                    if (scouringPad[ii, j] > totalMax)
+                    {
+                        xx = ii;
+                        yy = j;
+                        totalMax = scouringPad[ii,j];
+                        rhoMax = ii-range;
+                        thetaMax = j-90;
+                       
+                    }
+                }
+            }
+            double chang = totalMax * 0.4;
+            int[,] ccc= new int[1000, 3];
+            int c_count = 0;
+            for (int ii = 0; ii < 1 + 2 * range; ii++)
+            {
+                for (int j = 0; j < 181; j++)
+                {
+                    if (scouringPad[ii, j] > chang)
+                    {
+                        xx = ii;
+                        yy = j;
+                        totalMax = scouringPad[ii, j];
+                        rhoMax = ii - range;
+                        thetaMax = j - 90;
+                        int cc=0;
+                        int high;
+                        if (thetaMax == 0)
+                            break;
+                        for (int i = 0; i < a.Width; i++)
+                        {
+                            high = Convert.ToInt32((rhoMax - Math.Sin(thetaMax) * i) / Math.Cos(thetaMax));
+                            
+                            if (high >= 0)
+                            {
+                                if (high < a.Height)
+                                {
+                                    if (newimg1[i, high, 0] == 255)
+                                    {
+                                        cc++;
+                                        if(i==a.Width-1)
+                                        {
+                                            if (cc > 0.2 * range)
+                                            {
+                                                ccc[c_count, 0] = i;
+                                                ccc[c_count, 1] = i - cc;
+                                                ccc[c_count, 2] = cc;
+                                                c_count++;
+                                            }
+                                            cc = 0;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        if (cc > 0.2 * range)
+                                        {
+                                            ccc[c_count, 0] = i;
+                                            ccc[c_count, 1] = i - cc;
+                                            ccc[c_count, 2] = cc;
+                                            c_count++;
+                                        }
+                                        cc = 0;
+                                    }
+                                }
+                            }
+                        }
+                        
+                    }
+                }
+            }
+            int h;
+            for (int i = 0; i <= c_count; i++)
+            {
+                for (int pp = ccc[i, 1]; pp <= ccc[i, 0]; pp++)
+                {
+                    h = Convert.ToInt32((rhoMax - Math.Sin(thetaMax) * pp) / Math.Cos(thetaMax));
+                    if(h>=0)
+                    {
+                        if(h<a.Height)
+                        {
+                            source[pp, h, 0] = 255;
+                            source[pp, h, 1] = 0;
+                            source[pp, h, 2] = 0;
+                        }
+                    }
+                }
+            }
+            b = setRGBData(source);
+            pictureBox2.Image = b;
+            
+        }
+        int[,,] tomatoFilter(int[] k, int[,,] source)
+        {
+            int[,,] newimg = new int[source.GetLength(0), source.GetLength(1), 3];
+            int total, count;
+            for (int i = 1; i < a.Width - 1; i++)
+            {
+                for (int j = 1; j < a.Height - 1; j++)
+                {
+                    total = 0; count = 0;
+                    for (int p = i - 1; p <= i + 1; p++)
+                    {
+                        for (int q = j - 1; q <= j + 1; q++)
+                        {
+                            total += k[count] * (source[p, q, 0] + source[p, q, 1] + source[p, q, 2]) / 3;
+                            count++;
+                        }
+                    }
+                    if (total > 255)
+                        total = 255;
+                    else if (total < 0)
+                    {
+                        total = 0;
+                    }
+                    for (int p = 0; p < 3; p++)
+                        newimg[i, j, p] = total;
+                }
+            }
+            return newimg;
+        }
+        int[,,] meanFilter( int[,,] source)
+        {
+            int[,,] newimg = new int[source.GetLength(0), source.GetLength(1), 3];
+            int total, count;
+            for (int i = 1; i < a.Width - 1; i++)
+            {
+                for (int j = 1; j < a.Height - 1; j++)
+                {
+                    total = 0; count = 0;
+                    for (int p = i - 1; p <= i + 1; p++)
+                    {
+                        for (int q = j - 1; q <= j + 1; q++)
+                        {
+                            total +=  (source[p, q, 0] + source[p, q, 1] + source[p, q, 2]) / 3;
+                            count++;
+                        }
+                    }
+                    total /= 9;
+                    if (total > 255)
+                        total = 255;
+                    else if (total < 0)
+                    {
+                        total = 0;
+                    }
+                    for (int p = 0; p < 3; p++)
+                        newimg[i, j, p] = total;
+                }
+            }
+            return newimg;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public static int[,,] GetRGBData(Bitmap bitImg)
+        {
+            int height = bitImg.Height;
+            int width = bitImg.Width;
+            //locking
+            BitmapData bitmapData = bitImg.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+            // get the starting memory place
+            IntPtr imgPtr = bitmapData.Scan0;
+            //scan width
+            int stride = bitmapData.Stride;
+            //scan ectual
+            int widthByte = width * 3;
+            // the byte num of padding
+            int skipByte = stride - widthByte;
+            //set the place to save values
+            int[,,] rgbData = new int[width, height, 3];
+            #region
+            unsafe//專案－＞屬性－＞建置－＞容許Unsafe程式碼須選取。
+            {
+                byte* p = (byte*)(void*)imgPtr;
+                for (int j = 0; j < height; j++)
+                {
+                    for (int i = 0; i < width; i++)
+                    {
+                        //B channel
+                        rgbData[i, j, 2] = p[0];
+                        p++;
+                        //g channel
+                        rgbData[i, j, 1] = p[0];
+                        p++; 
+                        //R channel
+                        rgbData[i, j, 0] = p[0];
+                        p++;
+                    }
+                    p += skipByte;
+                }
+            }
+            bitImg.UnlockBits(bitmapData);
+            #endregion
+            return rgbData;
+        }
+        public static Bitmap setRGBData(int[,,] rgbData)
+        {
+            Bitmap bitImg;
+            int width = rgbData.GetLength(0);
+            int height = rgbData.GetLength(1);
+            bitImg = new Bitmap(width, height, PixelFormat.Format24bppRgb);// 24bit per pixel 8x8x8
+            //locking
+            BitmapData bitmapData = bitImg.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
+            //get image starting place
+            IntPtr imgPtr = bitmapData.Scan0;
+            //image scan width
+            int stride = bitmapData.Stride;
+            int widthByte = width * 3;
+            int skipByte = stride - widthByte;
+            #region
+            unsafe
+            {
+                byte* p = (byte*)(void*)imgPtr;
+                for (int j = 0; j < height; j++)
+                {
+                    for (int i = 0; i < width; i++)
+                    {
+                        p[0] = (byte)rgbData[i, j, 2];
+                        p++;
+                        p[0] = (byte)rgbData[i, j, 1];
+                        p++;
+                        p[0] = (byte)rgbData[i, j, 0];
+                        p++;
+                    }
+                    p += skipByte;
+                }
+
+            }
+            bitImg.UnlockBits(bitmapData);
+            #endregion
+            return bitImg;
+        }
+    }
+}
